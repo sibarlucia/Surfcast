8a2bb3830890e6fd3f9aacf6e3d72576a3a87394
Juan Carrero <juancarrero811@gmail.com>
2023-07-23 13:57:08 -0400

    add funnel component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,33 @@
 import { PageLayout } from '../components/General/PageLayout'  
 import { userLogoutAction } from '../Store/actions/user/userLogoutAction'
 import { useDispatch } from 'react-redux'
+import { Funnel } from '../components/Dashboards/Funnel/Index'
+import styles from '../styles/pages/home.module.css' 
 import '/src/styles/stylesglobales.css'
 
 // import rutas from '../Routes/routes'
 
 const Home = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const handleLogOut = () => {
-    dispatch(userLogoutAction())
-  }
+    const handleLogOut = () => { // eslint-disable-line
+        dispatch(userLogoutAction())
+    }
 
-  return (
-    <PageLayout>
-      <h1>
-        home
-      </h1>
-      <button onClick={handleLogOut}>
-        logout
-      </button>
-    </PageLayout>
+    return (
+        <PageLayout>
+            <h1>
+              home
+            </h1>
+            <div className={styles.funnelContainer}>
+                <Funnel></Funnel>
+            </div>
+            {/* <button onClick={handleLogOut}>
+              logout
+            </button> */}
+        </PageLayout>
     
-  ) 
+    ) 
 }
 
 
