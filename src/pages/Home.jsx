53f84aa41d66cec90ac3fdac9ecc06fa5ee993e0
sibarlucia <87153157+sibarlucia@users.noreply.github.com>
2023-07-24 18:08:15 -0300

    updateando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,52 @@
 import { PageLayout } from '../components/General/PageLayout'  
 import { userLogoutAction } from '../Store/actions/user/userLogoutAction'
 import { useDispatch } from 'react-redux'
-import '/src/styles/stylesglobales.css'
+import styles from '../styles/pages/campaignHome.module.css'
 
 // import rutas from '../Routes/routes'
 
 const Home = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const handleLogOut = () => {
-    dispatch(userLogoutAction())
-  }
+    const nombre = ''
 
-  return (
-    <PageLayout>
-      <h1>
-        home
-      </h1>
-      <button onClick={handleLogOut}>
+    const handleLogOut = () => {
+        dispatch(userLogoutAction())
+    }
+
+    return (
+        <PageLayout>
+            <article className={styles.mainArticle}>
+
+                <section id={styles.sectionLeft}>
+                    <h2>Hola, {nombre}</h2>
+                    <p>Esto es lo que está pasando con tu cuenta de LinkedIn hoy</p>
+
+                    <div id={styles.divLeft}>
+
+
+                    </div>
+                </section>
+
+                <section id={styles.sectionRight}>
+                    <div id={styles.divTopRight}>
+
+
+                    </div>
+
+                    <div id={styles.idottomRight}>
+
+
+                    </div>
+
+                </section>
+            </article>
+            {/* <button onClick={handleLogOut}>
         logout
-      </button>
-    </PageLayout>
+            </button> */}
+        </PageLayout>
     
-  ) 
+    ) 
 }
 
 
