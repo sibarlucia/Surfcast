12517c84dc3b5c772634a48f35b0ebc3984ab0b6
Juan Carrero <juancarrero811@gmail.com>
2023-07-31 09:39:01 -0400

    add styles for gmail history

--- src/components/campaign/GmailHistory/GmailHistory.jsx
+++ src/components/campaign/GmailHistory/GmailHistory.jsx
@@ -1,4 +1,6 @@
 import profileImg from '../../../assets/campaign/defaultProfile.svg'
+import styles from './index.module.css'
+
 const defaultHistory = [
     {
         user: {
@@ -33,41 +35,48 @@ const defaultHistory = [
     
 ]
 
-export const GmailHistory = ({ history = defaultHistory, leadId }) => {
+export const GmailHistory = ({ history = defaultHistory }) => {
     return (
-        <section>
-            <header>
-                <h1>
+        <section className={styles.mainsection}>
+            <header className={styles.mainHeader}>
+                <h1 className={styles.mainTitle}>
                     {/* cargar desde api de historial o campaña con el id */}
                     Fwd: Campaña Numero 1
-                </h1>
+                </h1> 
             </header>
-            <main>
-                <ul>
+            <main className={styles.mainContent}>
+                <ul className={styles.messageList}>
                     {
                         history.map((item, index) => {
                             return (
-                                <li key={`history-message-gmail-${index}`}>
-                                    <img src={item.user.icon} alt={item.user.name} />
-                                    <article>
-                                        <div>
-                                            <div>
+                                <li
+                                    key={`history-message-gmail-${index}`}
+                                    className={styles.messageItem}
+                                >
+                                    <img
+                                        src={item.user.icon}
+                                        alt={item.user.name}
+                                        className={styles.messageIcon}
+                                    />
+                                    <article className={styles.messageInfoContainer}>
+                                        <div className={styles.messageHeader}>
+                                            <div className={styles.messageUserInfo}>
                                                 <h2>
                                                     {item.user.name}
                                                     <span>{`<${item.user.mail}>`}</span>
                                                 </h2>
                                                 <div>
-                                                    para {item.for}
+                                                    Para {item.for}
                                                 </div>
                                             </div>
-                                            <div>
+                                            <div className={styles.messageActions}>
                                                 <p>
                                                     {item.date}
                                                 </p>
 
                                             </div>
                                         </div>
-                                        <p>
+                                        <p className={styles.messageText}>
                                             {item.message}
                                         </p>
                                     </article>
@@ -77,14 +86,14 @@ export const GmailHistory = ({ history = defaultHistory, leadId }) => {
                     }
                 </ul>
             </main>
-            {/* <footer>
-                <button>
+            <footer className={styles.footer}>
+                <button className={styles.footerButton}>
                     Responder
                 </button>
-                <button>
+                <button className={styles.footerButton}>
                     Reenviar
                 </button>
-            </footer> */}
+            </footer>
         </section>
     )
 }
