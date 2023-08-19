202cbdcc207674f415d08801ac6957455026600e
Juan Carrero <juancarrero811@gmail.com>
2023-08-19 00:19:06 -0400

    connect webinar 2 responses

--- src/components/General/WeekSchedule/index.jsx
+++ src/components/General/WeekSchedule/index.jsx
@@ -1,10 +1,7 @@
 import { useMemo } from 'react'
 import styles from './index.module.css'
 import { WeeksDays } from '../../../utils/getWeekDays'
-
-const hoursOptions = [
-    '00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00'
-] 
+import { hoursOptions } from '../../../utils/getHoursOptions'
 
 export const WeekSchedule = ({  onChange = () => {}, data = [], disabled = false }) => {
 
